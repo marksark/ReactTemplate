098e412737e6033d14a8713e263c585612c4bff2
Mark Sarkis <msarkis@211la.org>
2019-08-29 13:41:04 -0700

    adding refresher components again after doing only PHP for months

---
+++ src/comp/Comp.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class comp extends Component {
+    render() {
+        return (
+            <div style={{textAlign:'center'}}><input className='frog' name='mark' value='dawg'></input></div>
+        );
+    }
+}
+
+export default comp;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Comp from './comp/Comp';
 
 class App extends React.Component {
     render(){
@@ -7,6 +8,7 @@ class App extends React.Component {
             <React.Fragment>
                 <h1>Hello World</h1>
                 <p> This template was created to get a better understanding as to how babel/webpack work with react</p>
+                <Comp/>
             </React.Fragment>
         )
     }
